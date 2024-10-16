7c80ed92abfb498a812c978b73361672cd3bd098
David Mosyan <dmosyan@gmail.com>
2024-10-16 16:08:20 -0400

    microservices: add custom handler

--- microservices/main.go
+++ microservices/main.go
@@ -8,7 +8,16 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+	http.Handle("/", myHandler("Customer Service"))
+
+	var handlerFun http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, r.URL.String())
+	}
+
+	http.HandleFunc("/url/", handlerFun)
+
+	http.HandleFunc("/service/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Customer Service")
 	})
 
@@ -24,4 +33,10 @@ func main() {
 	fmt.Scanln()
 	s.Shutdown(context.Background())
 	fmt.Println("Server stopped")
+}
+
+type myHandler string
+
+func (mh myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, string(mh))
 }
