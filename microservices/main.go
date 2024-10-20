803fa536b38a1ace10d35a9c537247d3604fc0ff
David Mosyan <dmosyan@gmail.com>
2024-10-20 16:20:07 -0400

    microservices: send json data

--- microservices/main.go
+++ microservices/main.go
@@ -2,15 +2,38 @@ package main
 
 import (
 	"context"
+	"encoding/csv"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 )
 
 func main() {
+
+	http.HandleFunc("/customers", func(w http.ResponseWriter, r *http.Request) {
+		customers, err := readCustomers()
+		if err != nil {
+			log.Print(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		data, err := json.Marshal(customers)
+		if err != nil {
+			log.Print(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Add("content-type", "application/json")
+		w.Write(data)
+
+	})
 
 	http.Handle("/files/", http.StripPrefix("/files/", http.FileServer(http.Dir("."))))
 
@@ -88,4 +111,42 @@ func (mh myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Fprintln(w, string(mh))
 	fmt.Fprintln(w, r.Header)
+}
+
+type Customer struct {
+	ID        int    `json:"id"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Address   string `json:"address"`
+}
+
+func readCustomers() ([]Customer, error) {
+	f, err := os.Open("./testdata/customers.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	customers := make([]Customer, 0)
+	csvReader := csv.NewReader(f)
+	csvReader.Read() // throw away header
+	for {
+		fields, err := csvReader.Read()
+		if err == io.EOF {
+			return customers, nil
+		}
+		if err != nil {
+			return nil, err
+		}
+		var c Customer
+		id, err := strconv.Atoi(fields[0])
+		if err != nil {
+			continue
+		}
+		c.ID = id
+		c.FirstName = fields[1]
+		c.LastName = fields[2]
+		c.Address = fields[3]
+		customers = append(customers, c)
+	}
 }
