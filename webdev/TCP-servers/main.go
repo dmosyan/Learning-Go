ee01531dfe38ffea461fa6658a24d202df078c8f
David Mosyan <dmosyan@gmail.com>
2024-11-02 22:11:03 -0400

    webdev: add init TCP server

---
+++ webdev/TCP-servers/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+		}
+
+		io.WriteString(conn, "\nHello from TCP Server")
+		fmt.Fprintln(conn, "\nhow is your day?")
+		fmt.Fprintf(conn, "%v", "Well, I hope!")
+
+		conn.Close()
+	}
+
+}
