6bf3b19f4cb2a281a7b78d194cb6c43e891499ea
David Mosyan <dmosyan@gmail.com>
2024-11-06 20:39:45 -0500

    concurrency: mutex intro

---
+++ concurrency/mutex_channels/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var msg string
+var wg sync.WaitGroup
+
+func updateMessage(s string, m *sync.Mutex) {
+	defer wg.Done()
+
+	m.Lock()
+	msg = s
+	m.Unlock()
+}
+
+func main() {
+	msg = "Hello, there!"
+
+	var mutex sync.Mutex
+
+	wg.Add(2)
+
+	go updateMessage("Hello, world!", &mutex)
+	go updateMessage("Hello, universe!", &mutex)
+
+	wg.Wait()
+
+	fmt.Println(msg)
+}
