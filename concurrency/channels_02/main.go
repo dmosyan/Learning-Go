22ec72cb923bd4650d3cc51bd5a6659f04b8438a
David Mosyan <dmosyan@gmail.com>
2024-11-10 20:45:57 -0500

    concurrency: channel example 2

---
+++ concurrency/channels_02/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func shout(ping, pong chan string) {
+	for {
+		s := <-ping
+		pong <- fmt.Sprintf("%s!!!", strings.ToUpper(s))
+	}
+}
+
+func main() {
+	ping := make(chan string)
+	pong := make(chan string)
+
+	go shout(ping, pong)
+
+	fmt.Println("Type somthing and press ENTER (enter q to Quite)")
+
+	for {
+		fmt.Print("-> ")
+
+		var userInput string
+		_, _ = fmt.Scanln(&userInput)
+
+		if userInput == strings.ToLower("q") {
+			break
+		}
+
+		ping <- userInput
+		resposne := <-pong
+		fmt.Println("Response:", resposne)
+	}
+
+	fmt.Println("Closing channels")
+	close(ping)
+	close(pong)
+
+}
