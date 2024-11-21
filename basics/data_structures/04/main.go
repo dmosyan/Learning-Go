73a4b488c9d36c4f01d7b2ab82e1731132d8b42d
David Mosyan <dmosyan@gmail.com>
2024-11-20 21:20:10 -0500

    basics(04): use stack

--- basics/data_structures/04/main.go
+++ basics/data_structures/04/main.go
@@ -11,4 +11,13 @@ func main() {
 	myQueue.Dequeue()
 
 	fmt.Println(myQueue)
+
+	myStack := Stack{
+		elements: []any{1, 2, 3, 4, 5, "stack", 5.21},
+	}
+
+	myStack.Peek()
+	myStack.Pop()
+
+	fmt.Println(myStack)
 }
