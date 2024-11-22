5776598f2ac7feada46dd5de2d851191e2e34197
David Mosyan <dmosyan@gmail.com>
2024-11-22 16:03:37 -0500

    basics(04): add has copy and union to set

--- basics/data_structures/04/set.go
+++ basics/data_structures/04/set.go
@@ -29,3 +29,29 @@ func (s *Set) List() (list []any) {
 	}
 	return
 }
+
+func (s *Set) Has(el any) (ok bool) {
+	_, ok = s.elements[el]
+	return
+}
+
+func (s *Set) Copy() (u Set) {
+	u = NewSet()
+
+	for k := range s.elements {
+		u.Add(k)
+	}
+
+	return
+}
+
+func Union(sets ...Set) (u Set) {
+	u = sets[0].Copy()
+
+	for _, set := range sets[1:] {
+		for _, k := range set.elements {
+			u.Add(k)
+		}
+	}
+	return
+}
