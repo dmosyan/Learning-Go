8228ffb3bd6effc2da03404aa595dfb43d7c2a6a
David Mosyan <dmosyan@gmail.com>
2024-11-23 21:41:22 -0500

    modules: small setup changes

--- modules/main.go
+++ modules/main.go
@@ -11,15 +11,18 @@ func main() {
 
 	rtr := mux.NewRouter()
 
-	rtr.HandleFunc("/topics", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome to topics page"))
-	})
-
 	rtr.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Welcome to home page"))
 	})
 
-	err := http.ListenAndServe(":3000", rtr)
+	rtr.HandleFunc("/{topic}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		w.Write([]byte("Topic: " + vars["topic"]))
+	})
+
+	http.Handle("/", rtr)
+
+	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
