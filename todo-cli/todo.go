4c3a413a4e54365f59618e9f72453dfd1db0e7ef
David Mosyan <dmosyan@gmail.com>
2024-11-26 12:12:19 -0500

    todo-cli: add colors to the table

--- todo-cli/todo.go
+++ todo-cli/todo.go
@@ -100,10 +100,18 @@ func (t *Todos) Print() {
 
 	for idx, item := range *t {
 		idx++
+		task := blue(item.Task)
+		done := blue("no")
+
+		if item.Done {
+			task = green(fmt.Sprintf("\u2705 %s", item.Task))
+			done = green("yes")
+		}
+
 		cells = append(cells, []*simpletable.Cell{
 			{Text: fmt.Sprintf("%d", idx)},
-			{Text: item.Task},
-			{Text: fmt.Sprintf("%t", item.Done)},
+			{Text: task},
+			{Text: done},
 			{Text: item.CreatedAt.Format(time.RFC822)},
 			{Text: item.CompletedAt.Format(time.RFC822)},
 		})
@@ -111,10 +119,22 @@ func (t *Todos) Print() {
 
 	table.Body = &simpletable.Body{Cells: cells}
 	table.Footer = &simpletable.Footer{Cells: []*simpletable.Cell{
-		{Align: simpletable.AlignCenter, Span: 5, Text: "Todos list"},
+		{Align: simpletable.AlignCenter, Span: 5, Text: red(fmt.Sprintf("You have %d pending todos", t.CountPending()))},
 	}}
 
 	table.SetStyle(simpletable.StyleUnicode)
 	table.Println()
 
+}
+
+func (t *Todos) CountPending() int {
+	var pending int
+
+	for _, task := range *t {
+		if !task.Done {
+			pending++
+		}
+	}
+
+	return pending
 }
