1d7adaa94237e8589e9782557336387c0d83e307
David Mosyan <dmosyan@gmail.com>
2024-11-29 14:29:25 -0500

    basics(04): linkedlist add insert delete

--- basics/data_structures/04/linkedlist.go
+++ basics/data_structures/04/linkedlist.go
@@ -1,6 +1,10 @@
 package main
 
-import "golang.org/x/exp/constraints"
+import (
+	"errors"
+
+	"golang.org/x/exp/constraints"
+)
 
 type Element[T constraints.Ordered] struct {
 	value T
@@ -10,4 +14,56 @@ type Element[T constraints.Ordered] struct {
 type LinkedList[T constraints.Ordered] struct {
 	head *Element[T]
 	size int
+}
+
+func (l *LinkedList[T]) Add(el *Element[T]) {
+
+	if l.head == nil {
+		l.head = el
+	} else {
+		el.next = l.head
+		l.head = el
+	}
+
+	l.size++
+}
+
+func (l *LinkedList[T]) Insert(el *Element[T], marker T) error {
+
+	for current := l.head; current.next != nil; current = current.next {
+		if current.value == marker {
+			el.next = current.next
+			current.next = el
+			l.size++
+			return nil
+		}
+	}
+
+	return errors.New("element not found")
+
+}
+
+func (l *LinkedList[T]) Delete(el *Element[T]) error {
+
+	current := l.head
+	prev := l.head
+
+	for current != nil {
+
+		if current.value == el.value {
+			if current == l.head {
+				l.head = current.next
+			} else {
+				prev.next = current.next
+			}
+
+			l.size--
+			return nil
+		}
+
+		prev = current
+		current = current.next
+	}
+
+	return errors.New("element not found")
 }
