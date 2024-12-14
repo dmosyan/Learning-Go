e992d90958a98f0217403a8e1982e4aca1f83be4
David Mosyan <dmosyan@gmail.com>
2024-12-14 11:25:23 -0500

    apis(banking): add dto layer

--- apis/banking/service/customer_service.go
+++ apis/banking/service/customer_service.go
@@ -2,12 +2,13 @@ package service
 
 import (
 	"github.com/dmosyan/Learning-Go/apis/banking/domain"
+	"github.com/dmosyan/Learning-Go/apis/banking/dto"
 	"github.com/dmosyan/Learning-Go/apis/banking/errs"
 )
 
 type CustomerService interface {
 	GetAllCustomers(string) ([]domain.Customer, *errs.AppError)
-	GetCustomer(string) (*domain.Customer, *errs.AppError)
+	GetCustomer(string) (*dto.CustomerResponse, *errs.AppError)
 }
 
 type DefaultCustomerService struct {
@@ -25,8 +26,21 @@ func (s DefaultCustomerService) GetAllCustomers(status string) ([]domain.Custome
 	return s.repo.FindAll(status)
 }
 
-func (s DefaultCustomerService) GetCustomer(id string) (*domain.Customer, *errs.AppError) {
-	return s.repo.ById(id)
+func (s DefaultCustomerService) GetCustomer(id string) (*dto.CustomerResponse, *errs.AppError) {
+	c, err := s.repo.ById(id)
+	if err != nil {
+		return nil, err
+	}
+	resp := dto.CustomerResponse{
+		Id:          c.Id,
+		Name:        c.Name,
+		City:        c.City,
+		Zipcode:     c.Zipcode,
+		DateofBirth: c.DateofBirth,
+		Status:      c.Status,
+	}
+
+	return &resp, nil
 }
 
 func NewCustomerService(repository domain.CustomerRepository) DefaultCustomerService {
