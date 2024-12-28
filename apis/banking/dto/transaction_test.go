1054e1b194bdab44faba662a11ada9994ab5bcbb
David Mosyan <dmosyan@gmail.com>
2024-12-28 14:34:28 -0500

    apis(banking): add test for transaction amount

--- apis/banking/dto/transaction_test.go
+++ apis/banking/dto/transaction_test.go
@@ -23,3 +23,23 @@ func Test_should_return_error_when_transaction_type_is_not_deposit_or_withdrawl(
 		t.Error("Invalid code while testing transaction type")
 	}
 }
+
+func Test_should_return_error_when_amount_is_less_than_zero(t *testing.T) {
+	// Arrange
+	req := TransactionRequest{
+		TransactionType: DEPOSIT,
+		Amount:          -100,
+	}
+
+	// Act
+	appErr := req.Validate()
+
+	// Assert
+	if appErr.Message != "Amount cannot be less than zero" {
+		t.Error("Amount validation failed")
+	}
+
+	if appErr.Code != http.StatusUnprocessableEntity {
+		t.Error("Invalid code while testing amount")
+	}
+}
