968eea3aa804229ec582ba2a035d05b3394f2f3d
David Mosyan <dmosyan@gmail.com>
2025-01-12 23:14:15 -0500

    apis(social): add chi router middlewares

--- apis/social/cmd/api/app.go
+++ apis/social/cmd/api/app.go
@@ -4,6 +4,9 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 type application struct {
@@ -14,15 +17,29 @@ type config struct {
 	addr string
 }
 
-func (a *application) mount() *http.ServeMux {
-	mux := http.NewServeMux()
+func (a *application) mount() http.Handler {
+	r := chi.NewRouter()
 
-	mux.HandleFunc("GET /health", a.healthCheckHandler)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Logger)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 
-	return mux
+	// Set a timeout value on the request context (ctx), that will signal
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Route("/v1", func(r chi.Router) {
+		r.Get("/health", a.healthCheckHandler)
+	})
+
+	return r
 }
 
-func (a *application) run(mux *http.ServeMux) error {
+func (a *application) run(mux http.Handler) error {
 
 	srv := &http.Server{
 		Addr:         a.config.addr,
--- apis/social/go.mod
+++ apis/social/go.mod
@@ -1,3 +1,5 @@
 module github.com/dmosyan/Learning-Go/apis/social
 
 go 1.23.2
+
+require github.com/go-chi/chi/v5 v5.2.0 // indirect
