adbfcca8a26d38e44d9896923bb7c8ac9a73b9b7
David Mosyan <dmosyan@gmail.com>
2025-02-01 22:55:10 -0500

    apis(social): storage constructor and inject it into app

--- apis/social/cmd/api/main.go
+++ apis/social/cmd/api/main.go
@@ -4,15 +4,18 @@ import (
 	"log"
 
 	"github.com/dmosyan/Learning-Go/apis/social/internal/env"
+	"github.com/dmosyan/Learning-Go/apis/social/internal/store"
 )
 
 func main() {
 	cfg := config{
 		addr: env.GetEnv("PORT", ":3000"),
 	}
+	store := store.NewStorage(nil)
 
 	app := &application{
 		config: cfg,
+		store:  store,
 	}
 
 	mux := app.mount()
--- apis/social/internal/store/storage.go
+++ apis/social/internal/store/storage.go
@@ -1,6 +1,9 @@
 package store
 
-import "context"
+import (
+	"context"
+	"database/sql"
+)
 
 type Storage struct {
 	Posts interface {
@@ -8,5 +11,12 @@ type Storage struct {
 	}
 	Users interface {
 		Create(context.Context) error
+	}
+}
+
+func NewStorage(db *sql.DB) Storage {
+	return Storage{
+		Posts: &PostsStore{db: db},
+		Users: &UserStore{db: db},
 	}
 }
