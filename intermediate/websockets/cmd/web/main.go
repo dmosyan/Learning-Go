50d11680a47fbaf45584b224ed5393e05c8fd326
David Mosyan <dmosyan@gmail.com>
2025-02-17 18:22:26 -0500

    websockets: create home page and web server

--- intermediate/websockets/cmd/web/main.go
+++ intermediate/websockets/cmd/web/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
+
+	routes := routes()
+
+	log.Println("starting web server on :3000")
+
+	_ = http.ListenAndServe(":3000", routes)
 
 }
